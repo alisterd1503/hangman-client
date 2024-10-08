953521407d5ad9064a715e47222ff5b0a7da4bf7
alister <alisterdavid03@gmail.com>
2024-10-08 15:19:08 +0100

    Updated music icons in settings tab

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,7 +1,8 @@
-import { Box, IconButton, Slider, Stack, Typography } from "@mui/material";
+import { Box, Slider, Stack, Typography } from "@mui/material";
 import VolumeDown from '@mui/icons-material/VolumeDown';
 import VolumeUp from '@mui/icons-material/VolumeUp';
-import { VolumeOff } from "@mui/icons-material";
+import muteIcon from './images/mute.png';
+import unmuteIcon from './images/unmute.png';
 
 type SettingsProps = {
     volume: number;
@@ -17,24 +18,32 @@ export function Settings({
     setMute
 }: SettingsProps) {
 
+    // Handle volume change
     const handleChange = (_event: Event, newValue: number | number[]) => {
-        setVolume(newValue as number);
+        if (typeof newValue === 'number') {
+            setVolume(newValue);
+        }
     };
 
+    // Toggle mute state
+    const toggleMute = () => {
+        setMute(!mute);
+    }
+    console.log(mute,volume)
     return (
         <div style={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            height: "40vh"
+            height: "60vh"
         }}>
             <Typography variant="h1" style={{ marginBottom: "40px", fontFamily: "'Indie Flower', cursive", fontWeight: "bold", fontSize: "8rem" }}>
                 Settings
             </Typography>
 
-            <Box sx={{ width: 400 }}>
-                <Stack spacing={2} direction="row" sx={{ alignItems: 'center', mb: 1 }}>
+            <Box sx={{ width: 400, display: 'flex', flexDirection: 'column'}}>
+                <Stack spacing={2} direction="row" sx={{ alignItems: 'center', mb: 4 }}>
                     <VolumeDown />
                     <Slider
                         aria-label="Volume"
@@ -44,9 +53,31 @@ export function Settings({
                         max={100}
                     />
                     <VolumeUp />
-                    <IconButton onClick={() => setMute(!mute)}>
-                        {mute ? <VolumeOff /> : <VolumeUp />}
-                    </IconButton>
+                </Stack>
+
+                <Stack spacing={2} direction="row" sx={{ alignItems: 'center', mb: 1 }}>
+                        <Typography variant="h2" style={{ margin: 0, fontFamily: "'Indie Flower', cursive", fontWeight: "bold" }}>
+                            Mute
+                        </Typography>
+                        <img
+                            src={mute ? muteIcon : unmuteIcon}
+                            alt={mute ? "Unmute" : "Mute"}
+                            onClick={toggleMute}
+                            style={{
+                                width: '80px',
+                                height: '80px',
+                                cursor: 'pointer',
+                                transition: 'transform 0.3s, opacity 0.3s',
+                            }}
+                            onMouseEnter={(e) => {
+                                e.currentTarget.style.transform = 'scale(1.1)';
+                                e.currentTarget.style.opacity = '0.8';
+                            }}
+                            onMouseLeave={(e) => {
+                                e.currentTarget.style.transform = 'scale(1)';
+                                e.currentTarget.style.opacity = '1';
+                            }}
+                        />
                 </Stack>
             </Box>
         </div>
