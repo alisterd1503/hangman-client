745fb8f6f18097a438f03f204ed82e9fd2d37fdf
alister <alisterdavid03@gmail.com>
2024-10-22 12:35:18 +0100

    Added a AuthForm component

---
+++ src/components/pages/AuthForm.tsx
@@ -0,0 +1,207 @@
+import { Stack, Typography } from "@mui/material";
+import StyledCard from "../reusable/StyledCard";
+import openEyeIcon from "../../images/eyeOpen.png"
+import closedEyeIcon from "../../images/eyeClosed.png"
+import { SetStateAction, useState } from "react";
+
+const primaryColor = "#9c8b75"
+
+type AuthFormProps = {
+    handleUsernameChange: (event: { target: { value: SetStateAction<string>; }; }) => void
+    handlePasswordChange: (event: { target: { value: SetStateAction<string>; }; }) => void
+    autherisation: () => void,
+    message: string,
+    password: string,
+    username: string,
+    title: 'Register' | 'Login',
+    toggleVisibilty: boolean
+}
+
+
+export function AuthForm({
+    handlePasswordChange,
+    handleUsernameChange,
+    autherisation,
+    message,
+    password,
+    username,
+    title,
+    toggleVisibilty
+}: AuthFormProps) {
+    const [seePassword, setSeePassword] = useState(false) 
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh"
+        }}>
+            <StyledCard title={title} padding="20px" marginBottom="50px" fontSize="8rem" width="600px">
+                <Stack
+                    direction="column"
+                    spacing={-3}
+                    sx={{
+                        justifyContent: "center",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    {/* USERNAME */}
+                    <Stack direction="row" spacing={2} sx={{ justifyContent: "center", alignItems: "center" }}>
+                        <Typography variant="h3" style={{ marginBottom: "30px", fontFamily: "'Indie Flower', cursive", fontWeight: "bold", fontSize: "3rem" }}>
+                            Username:
+                        </Typography>
+                        <input
+                            onChange={handleUsernameChange}
+                            value={username}
+                            type="text"
+                            id={title.toLowerCase()}
+                            placeholder="Enter Username"
+                            autoComplete="off"
+                            style={{
+                                width: '250px',
+                                height: '50px',
+                                border: 'none',
+                                borderBottom: '2px solid black',
+                                fontFamily: "'Indie Flower', cursive",
+                                fontSize: "2rem",
+                                fontWeight: "bold",
+                                background: 'none',
+                                textAlign: 'center',
+                                marginBottom: '60px',
+                            }}
+                            onFocus={(event) => {
+                                event.target.style.outline = `none`;
+                                event.target.style.borderBottom = `2px solid ${primaryColor}`;
+                                event.target.placeholder = '';
+                            }}
+                            onBlur={(event) => {
+                                event.target.style.borderBottom = '2px solid black';
+                                event.target.placeholder = 'Enter Username';
+                            }}
+                        />
+                    </Stack>
+                    
+                    {/* PASSWORD */}
+                    <Stack direction="row" spacing={2} sx={{ justifyContent: "center", alignItems: "center" }}>
+                        <Typography variant="h3" style={{ marginBottom: "30px", fontFamily: "'Indie Flower', cursive", fontWeight: "bold", fontSize: "3rem" }}>
+                            Password:
+                        </Typography>
+                        <input
+                            onChange={handlePasswordChange}
+                            value={password}
+                            type={seePassword ? "text" : "password"}
+                            id={title.toLowerCase()}
+                            placeholder="Enter Password"
+                            autoComplete="off"
+                            style={{
+                                width: '250px',
+                                height: '50px',
+                                border: 'none',
+                                borderBottom: '2px solid black',
+                                fontFamily: "'Indie Flower', cursive",
+                                fontSize: "2rem",
+                                fontWeight: "bold",
+                                background: 'none',
+                                textAlign: 'center',
+                                marginBottom: '60px',
+                            }}
+                            onFocus={(event) => {
+                                event.target.style.outline = `none`;
+                                event.target.style.borderBottom = `2px solid ${primaryColor}`;
+                                event.target.placeholder = '';
+                            }}
+                            onBlur={(event) => {
+                                event.target.style.borderBottom = '2px solid black';
+                                event.target.placeholder = 'Enter Password';
+                            }}
+                        />
+                        {toggleVisibilty && 
+                        <>
+                            {seePassword ? 
+                                (<img
+                                    src={closedEyeIcon}
+                                    alt="See Password"
+                                    onClick={() => {setSeePassword(false)}}
+                                    style={{
+                                        width: '40px',
+                                        height: '40px',
+                                        cursor: 'pointer',
+                                        marginBottom: '50px',
+                                        transition: 'transform 0.3s, opacity 0.3s',
+                                    }}
+                                    onMouseEnter={(e) => {
+                                        e.currentTarget.style.transform = 'scale(1.1)';
+                                        e.currentTarget.style.opacity = '0.8';
+                                    }}
+                                    onMouseLeave={(e) => {
+                                        e.currentTarget.style.transform = 'scale(1)';
+                                        e.currentTarget.style.opacity = '1';
+                                    }}
+                                />) 
+                                :
+                                (<img
+                                    src={openEyeIcon}
+                                    alt="See Password"
+                                    onClick={() => {setSeePassword(true)}}
+                                    style={{
+                                        width: '40px',
+                                        height: '40px',
+                                        cursor: 'pointer',
+                                        marginBottom: '50px',
+                                        transition: 'transform 0.3s, opacity 0.3s',
+                                    }}
+                                    onMouseEnter={(e) => {
+                                        e.currentTarget.style.transform = 'scale(1.1)';
+                                        e.currentTarget.style.opacity = '0.8';
+                                    }}
+                                    onMouseLeave={(e) => {
+                                        e.currentTarget.style.transform = 'scale(1)';
+                                        e.currentTarget.style.opacity = '1';
+                                    }}
+                                />)
+                            }
+                        </>
+                        }
+                    </Stack>
+                </Stack>
+                <span
+                    onClick={autherisation}
+                    style={{
+                        color: primaryColor,
+                        display: "inline-block",
+                        fontSize: "4rem",
+                        height: "65px",
+                        fontFamily: "'Indie Flower', cursive",
+                        fontWeight: "bold",
+                        cursor: "pointer",
+                        transition: "transform 0.3s, background-color 0.3s",
+                        marginBottom: "20px"
+                    }}
+                    onMouseEnter={(e) => {
+                        e.currentTarget.style.color = 'black',
+                        e.currentTarget.style.transform = 'scale(1.1)'
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.style.color = primaryColor,
+                        e.currentTarget.style.transform = 'scale(1)'
+                    }}
+                    >
+                    SUBMIT
+                </span>
+            </StyledCard>
+
+            <div style={{transition: '0.3s', height: '50px'}}>
+            {message &&
+                <Typography
+                    style={{ 
+                        fontFamily: "'Indie Flower',cursive",
+                        fontWeight: "bold",
+                        fontSize: "2rem",
+                    }}>{message}
+                </Typography>
+                }
+            </div>
+        </div>
+    )
+}
--- src/components/pages/RegisterPage.tsx
+++ src/components/pages/RegisterPage.tsx
@@ -1,4 +1,3 @@
-import { Stack, Typography } from "@mui/material";
 import { SetStateAction, useEffect, useState } from "react";
 
 import { getUsernames } from '../../api/getUsernames';
@@ -6,16 +5,15 @@ import { addUser } from '../../api/registerUser'
 
 import { RegisterModel } from "../../models/RegisterModel";
 
-const primaryColor = "#9c8b75"
 
 import { getCountryByTimeZone } from '../../utils/getLocation';
 import { validatePassword } from "../../utils/validatePassword";
 import { validateUsername } from "../../utils/validateUsername";
-import StyledCard from "../reusable/StyledCard"
 
 import { clickSound } from "../sounds/clickSXF";
 import { play } from "../sounds/generalSFX";
 import error from '../../sounds/error.mp3'
+import { AuthForm } from "./AuthForm";
 
 const location: string = getCountryByTimeZone();
 
@@ -39,7 +37,23 @@ export function RegisterPage({navigateToLogin}:RegisterPageProps) {
         fetchScores();
     }, [])
 
-    const handleButtonClick = async () => {
+    const validateRegisteration = () => {
+        const passwordCheck = validatePassword(password)
+        const usernameCheck = validateUsername(username, usedNames)
+        if (passwordCheck.valid && usernameCheck.valid) {
+            clickSound()
+            valideRegisteration()
+        } else {
+            play(error)
+            if (usernameCheck.valid == false) {
+                setMessage(usernameCheck.message)
+            } else if (passwordCheck.valid == false) {
+                setMessage(passwordCheck.message)
+            }
+        }
+    }
+
+    const valideRegisteration = async () => {
         setMessage('')
         const body: RegisterModel = {
             username: username,
@@ -61,147 +75,15 @@ export function RegisterPage({navigateToLogin}:RegisterPageProps) {
     };
 
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh"
-        }}>
-            <StyledCard title="Register" padding="20px" marginBottom="50px" fontSize="8rem" width="600px">
-                <Stack
-                    direction="column"
-                    spacing={-3}
-                    sx={{
-                        justifyContent: "center",
-                        alignItems: "flex-start",
-                    }}
-                >
-                    {/* USERNAME */}
-                    <Stack direction="row" spacing={2} sx={{ justifyContent: "center", alignItems: "center" }}>
-                        <Typography variant="h3" style={{ marginBottom: "30px", fontFamily: "'Indie Flower', cursive", fontWeight: "bold", fontSize: "3rem" }}>
-                            Username:
-                        </Typography>
-                        <input
-                            onChange={handleUsernameChange}
-                            value={username}
-                            type="text"
-                            id="standard-basic"
-                            placeholder="Create Username"
-                            autoComplete="off"
-                            style={{
-                                width: '250px',
-                                height: '50px',
-                                border: 'none',
-                                borderBottom: '2px solid black',
-                                fontFamily: "'Indie Flower', cursive",
-                                fontSize: "2rem",
-                                fontWeight: "bold",
-                                background: 'none',
-                                textAlign: 'center',
-                                marginBottom: '60px',
-                            }}
-                            onFocus={(event) => {
-                                event.target.style.outline = `none`;
-                                event.target.style.borderBottom = `2px solid ${primaryColor}`;
-                                event.target.placeholder = '';
-                            }}
-                            onBlur={(event) => {
-                                event.target.style.borderBottom = '2px solid black';
-                                event.target.placeholder = 'Create Username';
-                            }}
-                        />
-                    </Stack>
-
-                    {/* PASSWORD */}
-                    <Stack direction="row" spacing={2} sx={{ justifyContent: "center", alignItems: "center" }}>
-                        <Typography variant="h3" style={{ marginBottom: "30px", fontFamily: "'Indie Flower', cursive", fontWeight: "bold", fontSize: "3rem" }}>
-                            Password:
-                        </Typography>
-                        <input
-                            onChange={handlePasswordChange}
-                            value={password}
-                            type="text"
-                            id="standard-basic"
-                            placeholder="Create Password"
-                            autoComplete="off"
-                            style={{
-                                width: '250px',
-                                height: '50px',
-                                border: 'none',
-                                borderBottom: '2px solid black',
-                                fontFamily: "'Indie Flower', cursive",
-                                fontSize: "2rem",
-                                fontWeight: "bold",
-                                background: 'none',
-                                textAlign: 'center',
-                                marginBottom: '60px',
-                            }}
-                            onFocus={(event) => {
-                                event.target.style.outline = `none`;
-                                event.target.style.borderBottom = `2px solid ${primaryColor}`;
-                                event.target.placeholder = '';
-                            }}
-                            onBlur={(event) => {
-                                event.target.style.borderBottom = '2px solid black';
-                                event.target.placeholder = 'Create Password';
-                            }}
-                        />
-                    </Stack>
-                </Stack>
-
-                <span
-                    onClick={() => {
-                        const passwordCheck = validatePassword(password)
-                        const usernameCheck = validateUsername(username, usedNames)
-
-                        if (passwordCheck.valid && usernameCheck.valid) {
-                            clickSound()
-                            handleButtonClick()
-                        } else {
-                            play(error)
-                            if (usernameCheck.valid == false) {
-                                setMessage(usernameCheck.message)
-                            } else if (passwordCheck.valid == false) {
-                                setMessage(passwordCheck.message)
-                            }
-                        }
-                    }} 
-                    style={{
-                        color: primaryColor,
-                        display: "inline-block",
-                        fontSize: "4rem",
-                        height: "65px",
-                        fontFamily: "'Indie Flower', cursive",
-                        fontWeight: "bold",
-                        cursor: "pointer",
-                        transition: "transform 0.3s, background-color 0.3s",
-                        marginBottom: "20px"
-                    }}
-                    onMouseEnter={(e) => {
-                        e.currentTarget.style.color = 'black',
-                        e.currentTarget.style.transform = 'scale(1.1)'
-                    }}
-                    onMouseLeave={(e) => {
-                        e.currentTarget.style.color = primaryColor,
-                        e.currentTarget.style.transform = 'scale(1)'
-                    }}
-                    >
-                    SUBMIT
-                </span>
-            </StyledCard>
-
-            <div style={{transition: '0.3s', height: '50px'}}>
-            {message &&
-                <Typography
-                    style={{ 
-                        fontFamily: "'Indie Flower',cursive",
-                        fontWeight: "bold",
-                        fontSize: "2rem",
-                    }}>{message}
-                </Typography>
-                }
-            </div> 
-        </div>
+        <AuthForm 
+            handlePasswordChange={handlePasswordChange}
+            handleUsernameChange={handleUsernameChange}
+            autherisation={validateRegisteration}
+            message={message}
+            title="Register"
+            username={username}
+            password={password}
+            toggleVisibilty={false}
+        ></AuthForm>
     )
 }
